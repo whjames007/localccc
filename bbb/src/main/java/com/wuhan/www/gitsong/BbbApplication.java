44455746f43ea0ed2115d903b67866ee66108a14
James Song <james.song007@hotmail.com>
2018-07-05 11:07:56 +0800

    bbb

---
+++ bbb/src/main/java/com/wuhan/www/gitsong/BbbApplication.java
@@ -0,0 +1,12 @@
+package com.wuhan.www.gitsong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbbApplication.class, args);
+	}
+}
